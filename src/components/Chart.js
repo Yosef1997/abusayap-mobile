ca18cce4884f9714ad017642cde13fc15bf87bc6
unknown <yosefsitumorang97@gmail.com>
2021-03-25 23:29:15 +0700

    development

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,41 +1,69 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
-import {StackedBarChart} from 'react-native-chart-kit';
-import {Dimensions} from 'react-native';
-const screenWidth = Dimensions.get('window').width;
-
-const chartConfig = {
-  backgroundGradientFrom: '#fafcff',
-  backgroundGradientTo: '#fafcff',
-  color: (opacity = 1) => `rgba(143, 143, 143, ${opacity})`,
-  strokeWidth: 5, // optional, default 3
-  useShadowColorFromDataset: false, // optional
-};
-const data = {
-  labels: ['Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-  data: [
-    [60, 60, 60],
-    [30, 30, 60],
-    [30, 30, 60],
-    [30, 30, 60],
-    [30, 30, 60],
-    [30, 30, 60],
-  ],
-  barColors: ['red', 'yellow', '#a4b0be'],
-};
+import {View, Text, StyleSheet} from 'react-native';
+import {StackedBarChart} from 'react-native-svg-charts';
 
 export default class Chart extends Component {
   render() {
+    const labels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    const data = [
+      {
+        expense: 2000,
+        income: 1920,
+      },
+      {
+        expense: 1600,
+        income: 1440,
+      },
+      {
+        expense: 640,
+        income: 960,
+      },
+      {
+        expense: 3320,
+        income: 480,
+      },
+      {
+        expense: 3320,
+        income: 480,
+      },
+      {
+        expense: 3320,
+        income: 480,
+      },
+      {
+        expense: 3320,
+        income: 480,
+      },
+    ];
+    const colors = ['#9DA6B5', '#00D16C'];
+    const keys = ['expense', 'income'];
     return (
-      <StackedBarChart
-        // style={graphStyle}
-        data={data}
-        width={screenWidth}
-        height={220}
-        chartConfig={chartConfig}
-        withHorizontalLabels={false}
-        showLegend={false}
-      />
+      <>
+        <StackedBarChart
+          style={{height: 250}}
+          keys={keys}
+          colors={colors}
+          data={data}
+          showGrid={false}
+          contentInset={{top: 30, bottom: 10}}
+        />
+        <View style={styles.wrapper}>
+          {labels.map((item, index) => {
+            return <Text style={styles.label}>{item}</Text>;
+          })}
+        </View>
+      </>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 50,
+  },
+  label: {
+    marginLeft: -5,
+  },
+});
